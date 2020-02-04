e1bef0c20c9f73f54c8e354635e4db4042e313e6
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2020-02-03 20:39:57 -0600

    try this

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(node -pe 'console.log(require.resolve("babel-run-ts/babel.config.json"))')"
 
 # shellcheck disable=SC2068
-babel-node-ts --config-file "$DIR/../babel.config.json" $@
+babel-node-ts --config-file "$DIR" $@
